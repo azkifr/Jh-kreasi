aee99ac3915372c19de9c7feea5226bc6a1b29fb
azkifr <azkifauz3012@gmail.com>
2026-09-18 14:07:08 +0700

    ci: add GitHub Actions workflow for automatic GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+let assetPrefix = "";
+let basePath = "";
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY?.replace(/.*?\//, "") || "Jh-kreasi";
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
+}
+
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: basePath,
+  assetPrefix: assetPrefix,
   images: {
     unoptimized: true,
   },
